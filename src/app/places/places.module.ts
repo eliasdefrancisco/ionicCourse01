5a5f7b3e618b2081b98a71dd8e229ab33be4fea4
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-08 21:09:44 +0200

    Tabs added and specific routing module

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -6,20 +6,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPage } from './places.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
-];
+import { PlacesRoutingModule } from './places-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    PlacesRoutingModule
   ],
   declarations: [PlacesPage]
 })
