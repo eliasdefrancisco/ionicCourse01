9338cab366df62e210b3ca064528cb3565caf842
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-14 18:57:12 +0200

    Spinner LoadingController

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -8,15 +9,30 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) { }
 
   ngOnInit() {
   }
 
   onLogin() {
     this.authService.login();
-    this.router.navigateByUrl('/places/tabs/discover');
+    this.isLoading = true;
+    this.loadingCtrl
+    .create({keyboardClose: true, message: 'Loggin in...'})
+    .then(loadingEl => {
+      loadingEl.present();
+      setTimeout(() => {
+        this.isLoading = false;
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/places/tabs/discover');
+      }, 3000);
+    });
   }
 
 }
