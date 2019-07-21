9e3ef7b7e83d485199d32ec522cf87c55ff8d9ac
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-21 12:18:08 +0200

    Creating and Fetching places in Firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,51 +1,92 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
+
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
-      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
-      149.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      'L\'Amour Toujours',
-      'A romantic place in Paris!',
-      'https://myldrwithafrenchman.files.wordpress.com/2017/01/eiffel_tower_in_paris__france_073036_.jpg?w=1118',
-      189.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip!',
-      'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
-      99.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    )
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
 
   get places() {
     return this._places.asObservable();
   }
 
-  constructor(private authService: AuthService) { }
+  // [
+  //   new Place(
+  //     'p1',
+  //     'Manhattan Mansion',
+  //     'In the heart of New York City.',
+  //     'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042533/Carnegie-Mansion-nyc.jpg',
+  //     149.99,
+  //     new Date('2019-01-01'),
+  //     new Date('2019-12-31'),
+  //     'abc'
+  //   ),
+  //   new Place(
+  //     'p2',
+  //     'L\'Amour Toujours',
+  //     'A romantic place in Paris!',
+  //     'https://myldrwithafrenchman.files.wordpress.com/2017/01/eiffel_tower_in_paris__france_073036_.jpg?w=1118',
+  //     189.99,
+  //     new Date('2019-01-01'),
+  //     new Date('2019-12-31'),
+  //     'abc'
+  //   ),
+  //   new Place(
+  //     'p3',
+  //     'The Foggy Palace',
+  //     'Not your average city trip!',
+  //     'https://i1.trekearth.com/photos/138102/dsc_0681.jpg',
+  //     99.99,
+  //     new Date('2019-01-01'),
+  //     new Date('2019-12-31'),
+  //     'abc'
+  //   )
+  // ]
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
+
+  fetchPlaces() {
+    return this.http
+    .get<{[key: string]: PlaceData}>('https://backend-stuff.firebaseio.com/offered-places.json')
+    .pipe(
+      map(resData => {
+        const places = [];
+        for (const key in resData) {
+          if (resData.hasOwnProperty(key)) {
+            places.push(new Place(
+              key,
+              resData[key].title,
+              resData[key].description,
+              resData[key].imageUrl,
+              resData[key].price,
+              new Date(resData[key].availableFrom),
+              new Date(resData[key].availableTo),
+              resData[key].userId
+            ));
+          }
+        }
+        return places;
+      }),
+      tap(places => {
+        this._places.next(places);
+      })
+    );
+  }
 
   getPlace(id: string) {
     return this.places.pipe(take(1), map(places => {
@@ -60,6 +101,7 @@ export class PlacesService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -70,10 +112,17 @@ export class PlacesService {
       dateTo,
       this.authService.userId
     );
-    return this.places.pipe(
+    return this.http.post<{name: string}>(
+      'https://backend-stuff.firebaseio.com/offered-places.json',
+      { ...newPlace, id: null}
+    ).pipe(
+      switchMap(resData => {
+        generatedId = resData.name;
+        return this.places;
+      }),
       take(1),
-      delay(2000),
       tap(places => {
+        newPlace.id = generatedId;
         this._places.next(places.concat(newPlace));
       })
     );
