9ff78116a563c41ea660a8b6959d3186601b151a
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-22 08:43:33 +0200

    Updating places in Firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -135,12 +135,12 @@ export class PlacesService {
     price: number,
     imageUrl: string
   ) {
+    let updatedPlaces: Place[];
     return this.places.pipe(
       take(1),
-      delay(2000),
-      tap(places => {
+      switchMap(places => {
         const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
-        const updatedPlaces = [...places];
+        updatedPlaces = [...places];
         const oldPlace = updatedPlaces[updatedPlaceIndex];
         updatedPlaces[updatedPlaceIndex] = new Place(
           oldPlace.id,
@@ -152,6 +152,12 @@ export class PlacesService {
           oldPlace.availableTo,
           oldPlace.userId
         );
+        return this.http.put(
+          `https://backend-stuff.firebaseio.com/offered-places/${placeId}.json`,
+          { ...updatedPlaces[updatedPlaceIndex], id: null }
+        );
+      }),
+      tap(() => {
         this._places.next(updatedPlaces);
       })
     );
