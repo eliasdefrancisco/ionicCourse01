fa5aa9e4f8a0df0e0e40a11dc0e463fcdcf0239d
Elias de Francisco Javaloyes <eliasdefrancisco@gmail.com>
2019-07-29 19:11:17 +0200

    Taking pictures from devices on new offer

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -52,6 +52,10 @@ export class NewOfferPage implements OnInit {
     this.form.patchValue({location: placeLocation});
   }
 
+  onImagePicked(imageData: string) {
+
+  }
+
   onCreateOffer() {
     if (!this.form.valid) { return; }
 
